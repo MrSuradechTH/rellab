e3d296b35f9b143f6f139e19057987fa30e331e7
MrSuradechTH <53972303+MrSuradechTH@users.noreply.github.com>
2023-01-15 16:19:03 +0700

    Update json.php

--- htdocs/json.php
+++ htdocs/json.php
@@ -1,12 +1,15 @@
 <?php
 	require_once 'connect_mysqli.php';
-	header('Content-Type: application/json');
+	// $date_dafault 
+	// $date = date("d-m-Y",strtotime('+6 hours, +543 year, -1 second'));
+	// echo $date;
 	$time = date("H:i:s",strtotime('+6 hours, +543 year, -1 second'));
 	// $query = mysqli_query($con, "SELECT ADDTIME(NOW(), '198327 0:0:0'),NOW()");
 	// $result = mysqli_fetch_array($query);
 	// echo($result[0].PHP_EOL.$result[1].PHP_EOL);
 	
 	if(isset($_POST['mode'])) {
+		header('Content-Type: application/json');
 		$mode = $_POST['mode'];
 		if ($mode == "update") {
 			if (isset($_POST['mode']) && isset($_POST['name']) && isset($_POST['temp']) && isset($_POST['humid'])) {
@@ -19,7 +22,7 @@
 				
 				$time_stamp = $result[0];
 				$old_array = explode("}",$result[1]);
-				echo count($old_array);
+				// echo count($old_array);
 				$size_max = 600 + 1;
 				if (count($old_array) > $size_max) {
 					// unset($old_array[count($old_array) - 1]);
@@ -39,7 +42,7 @@
 					$array = array("temp" => $temp, "humid" => $humid, "time" => $time);
 					$json = json_encode($array, JSON_PRETTY_PRINT);
 					$str = $json.",";
-					echo count($old_array);
+					// echo count($old_array);
 					for ($x = 0;$x < count($old_array);$x++) {
 						$str = $str.$old_array[$x]."}";
 					}
@@ -85,8 +88,38 @@
 				
 			}
 		}
-	}else if(isset($_GET['name'])) {{
-			$name = $_GET['name'];
+	}else if(isset($_GET['mode']) && isset($_GET['name'])) {
+		$mode = $_GET['mode'];
+		$name = $_GET['name'];
+		if ($mode == "getdatas") {
+			header('Content-Type:application/xls');  
+			header('Content-Disposition: attachment; filename=test.xls');
+			$query = mysqli_query($con, "SELECT LOG_600 FROM machine_data WHERE name = '$name'");
+			$result = mysqli_affected_rows($con);
+			$result = mysqli_fetch_array($query);
+			$old_array = explode("},",$result[0]);
+			// echo $old_array[1]."}";
+			
+			echo "<table><thead><tr><th>time</th><th>temp</th><th>humid</th></tr></thead><tbody>";
+			for ($x = 0;$x < count($old_array);$x++) {
+				// echo $x." ";
+				if ($x <= 0) {
+					$jsonarray = $old_array[count($old_array) - 1];
+					$jsondecode = json_decode($jsonarray);
+					// echo $jsondecode->time."   ".$jsondecode->temp."   ".$jsondecode->humid."   ".PHP_EOL;
+					echo "<tr><td>".$jsondecode->time."</td>"."<td>".$jsondecode->temp."</td>"."<td>".$jsondecode->humid."</td></tr>";
+				}else {
+					$jsonarray = $old_array[count($old_array) - 1 - $x]."}";
+					$jsondecode = json_decode($jsonarray);
+					// echo $jsondecode->time."   ".$jsondecode->temp."   ".$jsondecode->humid."   ".PHP_EOL;
+					echo "<tr><td>".$jsondecode->time."</td>"."<td>".$jsondecode->temp."</td>"."<td>".$jsondecode->humid."</td></tr>";
+				}
+			}
+			echo "</tbody></table>";
+			
+			// header('Content-Type:application/xls');  
+			// header('Content-Disposition: attachment; filename=test.xls');
+		}else {
 			$temp = strval(rand(25,180));
 			$humid = strval(rand(0,90));
 			
